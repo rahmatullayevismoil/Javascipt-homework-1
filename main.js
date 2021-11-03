aa51cfa450b5b6113e2af5538920adf6005acc08
rahmatullayevismoil <ismoilrahmatullayev000@gmail.com>
2021-11-03 09:37:00 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,37 @@
+var userName = prompt("Ismingizni kiriting");
+var userSurename = prompt("Famaliyangizni kiriting");
+var userFatherName = prompt("Otangizning ismini kiriting");
+var userAge = prompt("Yoshingizni kiriting");
+var userNation = prompt("Millatingizni kiriting");
+var userPlaceOfBirth = prompt("Tug'ilgan joyingizni kiriting");
+var userPassportNumber = prompt("Passport seriya raqamini kiriting");
+var userWorkOrStudy = prompt("Ishlaysizmi yoki O'qiysizmi?");
+var userAreYouMarried = prompt("Uylanganmisiz?");
+var userComment = prompt("Izohingizni kiriting");
+
+console.log(
+    "Ismingizni kiriting:" + " " + userName + " " + "\n" +
+    "Familiyangizni kiriting:" + " " + userSurename + " " + "\n" +
+    "Otangizning ismini kiriting:" + " " + userFatherName + " " + "\n" +
+    "Yoshingizni kiriting:" + " " + userAge + " " + "\n" +
+    "Millatingizni kiriting:" + " " + userNation + " " + "\n" +
+    "Tug`ilgan joyingizni kiriting:" + " " + userPlaceOfBirth + " " + "\n" +
+    "Passport seriya raqamini kiriting:" + " " + userPassportNumber + " " + "\n" +
+    "Ishlaysizmi yoki O'qiysizmi?:" + " " + userWorkOrStudy + " " + "\n" +
+    "Uylanganmisiz?:" + " " + userAreYouMarried + " " + "\n" +
+    "Izohingizni kiriting:" + " " + userComment + " " + "\n"
+)
+
+alert(
+    "Ismingizni kiriting:" + " " + userName + " " + "\n" +
+    "Familiyangizni kiriting:" + " " + userSurename + " " + "\n" +
+    "Otangizning ismini kiriting:" + " " + userFatherName + " " + "\n" +
+    "Yoshingizni kiriting:" + " " + userAge + " " + "\n" +
+    "Millatingizni kiriting:" + " " + userNation + " " + "\n" +
+    "Tug`ilgan joyingizni kiriting:" + " " + userPlaceOfBirth + " " + "\n" +
+    "Passport seriya raqamini kiriting:" + " " + userPassportNumber + " " + "\n" +
+    "Ishlaysizmi yoki O'qiysizmi?:" + " " + userWorkOrStudy + " " + "\n" +
+    "Uylanganmisiz?:" + " " + userAreYouMarried + " " + "\n" +
+    "Izohingizni kiriting:" + " " + userComment + " " + "\n"
+)
+
